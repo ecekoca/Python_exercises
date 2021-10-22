08730e707dab93b86d560e35e373ed34a3b37008
Ece Kocagoncu <ecekoca@gmail.com>
2021-10-22 16:31:55 +0100

    Update email_list.py

--- email_list.py
+++ email_list.py
@@ -8,12 +8,8 @@ Created on Fri Oct  8 23:17:52 2021
 def email_list(domains):
 	emails = []
 	for domain in domains:
-#		print(domain)
 	  for user in domains[domain]:
-#		  print(user)
 	    emails.append( user + '@' + domain)
-    
-	    #print(emails)
 	return(emails)
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
